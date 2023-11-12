33558652975eec8eaec7d44183c23128a137c949
banditpig <mike_k_houghton@yahoo.co.uk>
2023-11-12 08:49:54 +0000

    Now runs eval in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(dead_code)]
 
-use crate::parse::parse_expr;
+use std::collections::HashMap;
+use crate::eval::eval;
+use crate::parse::{parse_expr, parser};
 
 mod parse;
 mod eval;
@@ -8,6 +10,12 @@ mod util_parsers;
 
 fn main() {
     let txt = include_str!("../input.jj");
-    let (rest, res) = parse_expr(txt).unwrap();
-    dbg!(rest, res);
+    let (rest,  exprs) = parser(txt).unwrap();
+    let mut ctx = HashMap::new();
+    for expr in exprs{
+        //dbg!( &expr);
+        eval(expr, &mut ctx);
+    }
+
+    //dbg!(rest, res);
 }
